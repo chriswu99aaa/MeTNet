b4a892cf20cb961454f246a630a0db465949e320
Shuaimin <75012086+chriswu99aaa@users.noreply.github.com>
2023-12-11 11:03:10 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,18 +7,18 @@ dataset=fewcomm
 dataset_mode=IO
 N=5
 K=1
-python -u main.py --multi_margin --use_proto_as_neg --model MTNet --dataset $dataset --dataset_mode $dataset_mode --mode $mode --trainN $N --N $N --K $K --Q 1 --trainable_margin_init 6.5
+python -u main.py --multi_margin --use_proto_as_neg --model MTNet --dataset $dataset --dataset_mode $dataset_mode  --trainN $N --N $N --K $K --Q 1 --trainable_margin_init 6.5
 
 K=5
-python -u main.py --multi_margin --use_proto_as_neg --model MTNet --dataset $dataset --dataset_mode $dataset_mode --mode $mode --trainN $N --N $N --K $K --Q 1 --trainable_margin_init 7.3
+python -u main.py --multi_margin --use_proto_as_neg --model MTNet --dataset $dataset --dataset_mode $dataset_mode  --trainN $N --N $N --K $K --Q 1 --trainable_margin_init 7.3
 
 N=10
 K=1
-python -u main.py --multi_margin --use_proto_as_neg --model MTNet --dataset $dataset --dataset_mode $dataset_mode --mode $mode --trainN $N --N $N --K $K --Q 1 --trainable_margin_init 6.1
+python -u main.py --multi_margin --use_proto_as_neg --model MTNet --dataset $dataset --dataset_mode $dataset_mode  --trainN $N --N $N --K $K --Q 1 --trainable_margin_init 6.1
 
 N=10
 K=5
-python -u main.py --multi_margin --use_proto_as_neg --model MTNet --dataset $dataset --dataset_mode $dataset_mode --mode $mode --trainN $N --N $N --K $K --Q 1 --trainable_margin_init 6.4
+python -u main.py --multi_margin --use_proto_as_neg --model MTNet --dataset $dataset --dataset_mode $dataset_mode --trainN $N --N $N --K $K --Q 1 --trainable_margin_init 6.4
 
 endTime=`date +%Y%m%d-%H:%M`
 endTime_s=`date +%s`
