2b0c0d812339bd419efaf3d70ab03e3a605fbc65
f99767sw <shuaimin.wu@student.manchester.ac.uk>
2024-02-29 15:52:10 +0000

    change fc attribute

--- model/bert.py
+++ model/bert.py
@@ -22,7 +22,7 @@ class BertNERModel(utils.framework.FewShotNERModel):
         self.bert = BertModel.from_pretrained('bert-base-chinese')
         self.dropout = nn.Dropout(0.1) 
         # output layer
-        self.linear1 = nn.Linear(self.bert.config.hidden_size, 111)# 111 number of classes
+        self.fc= nn.Linear(self.bert.config.hidden_size, 111)# 111 number of classes
         self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, ids, mask):
