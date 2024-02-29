ffcb76ad41dc8cb2af1ab2b247292177813a2d1e
f99767sw <shuaimin.wu@student.manchester.ac.uk>
2024-02-29 22:30:54 +0000

    check dataset

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ def main():
         framework = FewShotNERFramework(opt, tokenizer, train_data_loader, val_data_loader, test_data_loader, use_sampled_data=opt.use_sampled_data)
     elif model_name == 'bert':
         print('use BERT model')
-        # print("dataloader: ", train_data_loader.dataset.classes)
+        print("dataloader: ", dir(train_data_loader.dataset))
         num_classes = len(train_data_loader.dataset.classes)
         model = BertNERModel(opt,num_classes, word_encoder, dot=opt.dot, ignore_index=opt.ignore_index)
         framework = FewShotNERFramework(opt, tokenizer, train_data_loader, val_data_loader, test_data_loader, use_sampled_data=opt.use_sampled_data)
